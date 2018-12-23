e835d1bcb60ba66ace766a6683e19c6d9bf9b3c7
AliEdalat <ali.edalat.77@gmail.com>
2018-12-23 22:29:09 +0330

    schtest is updated.

--- schtest.c
+++ schtest.c
@@ -1,42 +1,3 @@
-// #include "types.h"
-// #include "stat.h"
-// #include "user.h"
-// #include "fcntl.h"
-// #include "syscall.h"
-
-// int
-// main(int argc, char* argv[])
-// {
-//     int pid = 1;
-//     // int j = 0;
-//     //chmfq(getpid(),3);
-//     //chpr(getpid(),1);
-//     int i;
-//     int z;
-//     for(i = 0; i< 15; i++){
-//         if(pid>0)
-//             pid=fork();
-//     }
-//     if(pid<0)
-//         printf(1,"error\n");
-//     else if(pid == 0){
-//         //chmfq(getpid(),3);
-//         // ps();
-//         for (z = 0; z < 10000; z+=1){
-//             int s;
-//             s = uptime();
-//             while((uptime() - s) > 100){
-//                 ;
-//             }
-//         }
-//     }
-//     else {
-//         for(i = 0; i< 15; i++)
-//             wait();
-//         printf(1,"finished:D\n");
-//     }
-//     exit();
-// }
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -61,10 +22,10 @@ main(int argc, char *argv[])
     if ( id < 0 ) {
       printf(1, "%d failed in fork!\n", getpid() );
     } else if ( id > 0 ) {  //parent
-      printf(1, "Parent %d creating child  %d\n", getpid(), id );
+      // printf(1, "Parent %d creating child  %d\n", getpid(), id );
       wait ();
    } else {   // child
-      printf(1, "Child %d created\n",getpid() );
+      // printf(1, "Child %d created\n",getpid() );
       for ( z = 0; z < 8000000.0; z += 0.01 )
          x =  x + 3.14 * 89.64;   // useless calculations to consume CPU time
       break;
